b886494cd2288a353f1b3599f2dd1757fe9804b2
Kate Konovalova <kate.konovalova.qa@gmail.com>
2022-12-28 23:50:18 +0200

    Palindrom

--- src/main/java/Palindrom.java
+++ src/main/java/Palindrom.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Palindrom {
+import java.util.Scanner; // импорт сканера
+
+class Palindrom {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите любое слово или фразу: ");
+        String phrase1 = sc.nextLine();
+        System.out.println(phrase1);
+        StringBuffer rev = new StringBuffer(phrase1).reverse();
+        String strRev = rev.toString();
+        if (phrase1.equalsIgnoreCase(strRev)) {
+            System.out.println("Это полиндром");
+        } else {
+            System.out.println("Не полиндром");
+        }
+
+
+    }
 }
